d217294a14171739db8580686342f3e831d5fa15
cdvlpr <contacto.dvlpr@protonmail.com>
2021-02-11 17:35:44 +0100

    0.5-Route Done

--- server/bin/seed.js
+++ server/bin/seed.js
@@ -13,7 +13,6 @@ Phone.collection.drop()
 
 const phoneList = [
     {
-      "id": 0,
       "name": "iPhone 7",
       "manufacturer": "Apple",
       "description": "iPhone 7 dramatically improves the most important aspects of the iPhone experience. It introduces advanced new camera systems. The best performance and battery life ever in an iPhone. Immersive stereo speakers. The brightest, most colorful iPhone display. Splash and water resistance*. And it looks every bit as powerful as it is. This is iPhone 7.",
@@ -25,7 +24,6 @@ const phoneList = [
       "ram": 2
   },
   {
-      "id": 1,
       "name": "Galaxy S7",
       "manufacturer": "Samsung",
       "description": "Introducing the smartphone your life can not do without, The Samsung Galaxy S7. Packed with features to keep you both productive and entertained, all in a sleek, slim design that fits comfortably in your hand or pocket.",
@@ -37,7 +35,6 @@ const phoneList = [
       "ram": 4
   },
   {
-      "id": 2,
       "name": "Honor 10",
       "manufacturer": "Huawei",
       "description": "Great phone with an excellent interface. One of the best mid price range phones in the market.",
@@ -49,7 +46,6 @@ const phoneList = [
       "ram": 6
   },
   {
-      "id": 3,
       "name": "P10 Lite",
       "manufacturer": "Huawei",
       "description": "Great phone with an excellent interface. One of the best mid price range phones in the market.",
@@ -61,7 +57,6 @@ const phoneList = [
       "ram": 4
   },
   {
-      "id": 4,
       "name": "Nokia 7.1",
       "manufacturer": "Nokia",
       "description": "Great phone with an excellent interface. One of the best mid price range phones in the market.",
@@ -73,7 +68,6 @@ const phoneList = [
       "ram": 4
   },
   {
-      "id": 5,
       "name": "Zen Phone 5",
       "manufacturer": "Asus",
       "description": "Great phone with an excellent interface. One of the best mid price range phones in the market.",
@@ -85,7 +79,6 @@ const phoneList = [
       "ram": 6
   },
   {
-      "id": 6,
       "name": "MI A2",
       "manufacturer": "Xiaomi",
       "description": "Great phone with an excellent interface. One of the best mid price range phones in the market.",
@@ -97,7 +90,6 @@ const phoneList = [
       "ram": 6
   },
   {
-      "id": 7,
       "name": "Moto G6",
       "manufacturer": "Motorola",
       "description": "Great phone with an excellent interface. One of the best mid price range phones in the market.",
--- server/routes/phone.routes.js
+++ server/routes/phone.routes.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose')
 
 const Phone = require('../models/phone.model')
 const checkId = require('../middlewares/middlewares')
+const { response } = require('express')
 
 
 
@@ -19,12 +20,29 @@ router.get('/getAllPhones', (req, res) => {
 
 router.get('/getPhone/:id', checkId, (req, res) => {
 
-    Phone.findById(req.params._id)
-         .then(response => res.json(response))
-         .catch(err => res.status(500).json(err))
+    Phone
+        .findById(req.params.id)
+        .catch(err => res.status(500).json(err))
+        .then(response => res.json(response))
+})
+
+    
+router.post('/newPhone', (req, res) => {
+
+    Phone
+        .create(req.body)
+        .then(response => res.json(response))
+        .catch(err => res.status(500).json(err))
 })
 
 
+router.put('/editPhone/:id', checkId, (req, res) => {
+    
+    Phone
+        .findByIdAndUpdate(req.params.id, req.body)
+        .then(response => res.json(response))
+        .catch(err => res.status(500).json(err))
+})
 
 
 module.exports = router
